4f9c6239e2215ffb62405fef411c59253d61542d
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-05-08 17:07:08 +0300

    deployment

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from . import db
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+from . import db
 
 @login_manager.user_loader
 
@@ -87,5 +88,3 @@ class Comments(db.Model):
 
 
   
-
-
